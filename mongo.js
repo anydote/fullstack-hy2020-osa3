d8a0b9f233c09017f086e5bd1ab0b8f3e81bb6ba
Antti <antti.maatta@gmail.com>
2020-04-19 20:30:26 +0300

    Tietokanta komentoriviltä (3.12)

---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://dbwriter:${password}@fullstack-open-hy2020-izmzr.mongodb.net/phonebook?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+switch (process.argv.length) {
+  case 3:
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+      result.forEach(person => {
+        console.log(person.name, person.number)
+      })
+      mongoose.connection.close()
+    })    
+    break
+  case 4:
+    console.log('Give both a name and a number')
+    process.exit(1)
+  case 5:
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4]
+    })
+    
+    person.save().then(response => {
+      console.log(`Added ${person.name} ${person.number} to phonebook`)
+      mongoose.connection.close()
+    })
+  default:
+    console.log('Too many parameters')
+    process.exit(1)
+}
