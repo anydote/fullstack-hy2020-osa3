84ef824383a81c8b9c743848ff54736de7c9d401
Antti <antti.maatta@gmail.com>
2020-04-19 14:56:12 +0300

    Puhelinluettelon backend, osa 2 (3.2)

--- index.js
+++ index.js
@@ -4,34 +4,38 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-  {
-    "persons":[
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
   }
 ]
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const now = new Date()
+  res.send(`
+    <p>The phonebook contains info about ${persons.length} people.</p>
+    <p>${now}</p>
+  `)
 })
 
 const PORT = 3001
