7563897f05d7f6743fe0f5d38a6baa9c38ef9cbc
Antti <antti.maatta@gmail.com>
2020-04-19 17:14:43 +0300

    uibuild

---
+++ build/precache-manifest.15d72f47d9265a578510f1dc532a3a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e082a1091c9f3512242553788ca041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ab49d322fd7c8f263e6",
+    "url": "/static/css/main.8ab99a36.chunk.css"
+  },
+  {
+    "revision": "c816bcf7231dd4c169fa",
+    "url": "/static/js/2.e7311442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7311442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab49d322fd7c8f263e6",
+    "url": "/static/js/main.ce202eac.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
